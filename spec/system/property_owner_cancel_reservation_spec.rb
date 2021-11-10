964670dfec7a289ae77d722bc014c1710a15da19
angelasoler <manzanaembicicleta@gmail.com>
2021-11-10 18:05:00 -0300

    add botao ver reserva e enviar ao teste

--- spec/system/property_owner_cancel_reservation_spec.rb
+++ spec/system/property_owner_cancel_reservation_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe 'Owner view own reservation' do
   it 'and cancel same day of start reservation' do
     owner = create(:property_owner, email: 'alessandro@owner.com', password: '123456')
-    user = create(:user, email: 'angelas@user.com', password: 'dsfgsd51 -- ,')
     property = create(:property, title: 'Casa na Alvora, Manaus AM', property_owner: owner)
     reservation = create(:property_reservation, property: property, status: 'accepted' )
   
@@ -11,7 +10,7 @@ describe 'Owner view own reservation' do
     visit root_path
     click_on 'Meus Imóveis'
     click_on 'Casa na Alvora, Manaus AM'
-    click_on 'angelas@user.com'
+    click_on 'Ver Reserva'
     click_on 'Cancelar Reserva'
 
     expect(page).to have_content('Reserva cancelada com sucesso!')
@@ -21,7 +20,8 @@ describe 'Owner view own reservation' do
     expect(page).not_to have_content('Status: Aceito')
     expect(page).not_to have_content('Status: Rejeitado')
     expect(page).not_to have_content('Status: Pendente')
-    expect(page).to have_content('Statu: Reserva cancelada')
+    expect(page).to have_content('Status: Reserva cancelada')
     expect(page).to have_content('Escreva porque vôce cancelou')
+    expect(page).to have_link('Enviar')
   end
 end
